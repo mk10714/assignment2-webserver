07f1a9fe7daf9eee64694bea2f7dac7b6ad88a3a
mk10714 <mk10714@nyu.edu>
2026-02-14 16:17:29 -0500

    webServer.py

--- webServer.py
+++ webServer.py
@@ -1,95 +1,97 @@
-#webserver.py
+#webserver2.py
 
+# webserver.py
 # import socket module
 from socket import *
 # In order to terminate the program
 import sys
 from datetime import datetime
-#import rfc3339
 
+# import rfc3339
 
 
 def webServer(port=13331):
-  serverSocket = socket(AF_INET, SOCK_STREAM)
-  
-  #Prepare a server socket
-  serverSocket.bind(("", port)) #Fill in start
-  serverSocket.listen(1)
+    serverSocket = socket(AF_INET, SOCK_STREAM)
+
+    # Prepare a server socket
+    serverSocket.bind(("", port))  # Fill in start
+    serverSocket.listen(1)  # Fill in end
 
     while True:
-    #Establish the connection
-      print('Ready to serve...')
-      connectionSocket, addr = serverSocket.accept() #Fill in start -are you accepting connections?     #Fill in end
-    
-      try:
-        message = connectionSocket.recv(1024).decode() #Fill in start -a client is sending you a message   #Fill in end 
-        filename = message.split()[1]
-         
-        #opens the client requested file. 
-        #Plenty of guidance online on how to open and read a file in python. How should you read it though if you plan on sending it through a socket?
-        f = open(filename[1:], 'rb')     #fill in start              #fill in end   )
-        
-        
-
-        #This variable can store the headers you want to send for any valid or invalid request.   What header should be sent for a response that is ok?    
-        #Fill in start 
-                
-        #Content-Type is an example on how to send a header as bytes. There are more!
-
-        outputdata = b"HTTP/1.1 200 OK\r\n"
-
-        now_utc = datetime.utcnow()
-        date_utc = now_utc.strftime('%a, %d %b %Y %H:%M:%S GMT')
-
-        outputdata = outputdata + b"Date: " + date_utc.encode() + b"\r\n"
-        outputdata = outputdata + b"Server: Apache/2.4.62 (AlmaLinux) OpenSSL/3.5.1 mod_fcgid/2.3.9 mod_perl/2.0.12 Perl/v5.32.1\r\n"
-        outputdata = outputdata + b"Content-Type: text/html; charset=UTF-8\r\n"
-        #outputdata = outputdata + b"Connection: close\r\n"
-        #outputdata = outputdata + b"Connection: keep-alive\r\n"
-        outputdata = outputdata + b"\r\n"
-
-        #Note that a complete header must end with a blank line, creating the four-byte sequence "\r\n\r\n" Refer to https://w3.cs.jmu.edu/kirkpams/OpenCSF/Books/csf/html/TCPSockets.html
-   
-        #Fill in end
-               
-        for i in f: #for line in file
-        #Fill in start - append your html file contents #Fill in end 
-        	outputdata = outputdata + i
-          
-        #Send the content of the requested file to the client (don't forget the headers you created)!
-        #Send everything as one send command, do not send one line/item at a time!
-
-        # Fill in start    	
-        	connectionSocket.send(outputdata)
-
+        # Establish the connection
+        print('Ready to serve...')
+        connectionSocket, addr = serverSocket.accept()  # Fill in start - are you accepting connections?
         # Fill in end
-          
-        connectionSocket.close() #closing the connection socket
-        f.close()
-      
-      except Exception as e:
-        # Send response message for invalid request due to the file not being found (404)
-        # Remember the format you used in the try: block!
-        #Fill in start
-        
 
-        outputdata_notfound = b"HTTP/1.1 404 Not Found\r\n"
-        
-        outputdata_notfound = outputdata_notfound + b"Content-Type: text/html; charset=UTF-8\r\n\r\n"
-        outputdata_notfound = outputdata_notfound + b"<html><head></head><body><h1>404 Not Found</h1></body></html>"
+        try:
+            message = connectionSocket.recv(1024).decode()  # Fill in start - a client is sending you a message
+            # Fill in end
+            filename = message.split()[1]
 
-        connectionSocket.send(outputdata_notfound)
-        #Fill in end
-    
-        #Close client socket
-        #Fill in start
-        connectionSocket.close() #closing the connection socket
-        #Fill in end
+            # Opens the client requested file.
+            # Plenty of guidance online on how to open and read a file in python.
+            # How should you read it though if you plan on sending it through a socket?
+            f = open(filename[1:], 'rb')  # fill in start
+            # fill in end
 
-  # Commenting out the below (some use it for local testing). It is not required for Gradescope, and some students have moved it erroneously in the While loop. 
-  # DO NOT PLACE ANYWHERE ELSE AND DO NOT UNCOMMENT WHEN SUBMITTING, YOU ARE GONNA HAVE A BAD TIME
-  #serverSocket.close()
-  #sys.exit()  # Terminate the program after sending the corresponding data
+            # This variable can store the headers you want to send for any valid or invalid request.
+            # What header should be sent for a response that is ok?
+            # Fill in start
+            # Content-Type is an example on how to send a header as bytes. There are more!
+            outputdata = b"HTTP/1.1 200 OK\r\n"
+
+            now_utc = datetime.utcnow()
+            date_utc = now_utc.strftime('%a, %d %b %Y %H:%M:%S GMT')
+            # CHANGE 3: Added .encode() to date_utc and changed "\r\n" to b"\r\n". Cant concatenate bytes (b"Date: ") with string (date_utc). You need to encode the string to bytes
+            outputdata = outputdata + b"Date: " + date_utc.encode() + b"\r\n"
+            outputdata = outputdata + b"Server: Apache/2.4.62 (AlmaLinux) OpenSSL/3.5.1 mod_fcgid/2.3.9 mod_perl/2.0.12 Perl/v5.32.1\r\n"
+            outputdata = outputdata + b"Content-Type: text/html; charset=UTF-8\r\n"
+            # outputdata = outputdata + b"Connection: close\r\n"
+            # outputdata = outputdata + b"Connection: keep-alive\r\n"
+            outputdata = outputdata + b"\r\n"
+            # Note that a complete header must end with a blank line, creating the four-byte sequence "\r\n\r\n"
+            # Refer to https://w3.cs.jmu.edu/kirkpams/OpenCSF/Books/csf/html/TCPSockets.html
+            # Fill in end
+
+            # Read file content
+            for i in f:  # for line in file
+                # Fill in start - append your html file contents
+                # Fill in end
+                outputdata = outputdata + i
+
+            # Send the content of the requested file to the client (don't forget the headers you created)!
+            # Send everything as one send command, do not send one line/item at a time!
+            # Fill in start
+            connectionSocket.send(outputdata)
+            # Fill in end
+
+            connectionSocket.close()  # closing the connection socket
+            f.close()
+
+        except Exception as e:
+            # Send response message for invalid request due to the file not being found (404)
+            # Remember the format you used in the try: block!
+            # Fill in start
+            outputdata_notfound = b"HTTP/1.1 404 Not Found\r\n"
+            outputdata_notfound = outputdata_notfound + b"Content-Type: text/html; charset=UTF-8\r\n\r\n"
+            outputdata_notfound = outputdata_notfound + b"<html><head></head><body><h1>404 Not Found</h1></body></html>"
+            connectionSocket.send(outputdata_notfound)
+            # Fill in end
+
+            # Close client socket
+            # Fill in start
+            connectionSocket.close()  # closing the connection socket
+            # Fill in end
+
+    # Commenting out the below (some use it for local testing).
+    # It is not required for Gradescope, and some students have moved it erroneously in the While loop.
+    # DO NOT PLACE ANYWHERE ELSE AND DO NOT UNCOMMENT WHEN SUBMITTING, YOU ARE GONNA HAVE A BAD TIME
+    # serverSocket.close()
+    # sys.exit()  # Terminate the program after sending the corresponding data
+
 
 if __name__ == "__main__":
-  webServer(13331)
+    webServer(13331)
+
+
+
